49276ee6d049f88e2f102a01958194309e2b9a7b
IScreates <shreeyashshinde76@gmail.com>
2025-09-29 11:27:51 +0530

    Task 3 Completed..

--- TNS_Java_Training/src/com/shreeyash/constructor/studentDeatilsDemo.java
+++ TNS_Java_Training/src/com/shreeyash/constructor/studentDeatilsDemo.java
@@ -4,7 +4,7 @@ public class studentDeatilsDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		studentDetails s1=new studentDetails("Shreeyash",12,78.00,"A");
+		studentDetails s1=new studentDetails("Shreeyash",12,78.00,"A",121);
 	}
 
 }
--- TNS_Java_Training/src/com/shreeyash/constructor/studentDetails.java
+++ TNS_Java_Training/src/com/shreeyash/constructor/studentDetails.java
@@ -1,12 +1,14 @@
 package com.shreeyash.constructor;
 
 public class studentDetails {
-	studentDetails(String Name,int rollNumber,double percent,String grade)
+	studentDetails(String Name,int rollNumber,double percent,String grade,int libId)
 	{
 		System.out.println("==================Parameterized Constructor============");
 		System.out.println("Name is :" +Name);
 		System.out.println("Roll Number is :"+rollNumber);
 		System.out.println("Percentage is :"+percent);
 		System.out.println("Grade is :"+grade);
+		System.out.println("Library Id :"+libId);
+		
 	}
 }
