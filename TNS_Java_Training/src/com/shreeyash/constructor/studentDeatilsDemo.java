4ff13ae3b20e1d11db16421d317632a34c1443c3
IScreates <shreeyashshinde76@gmail.com>
2025-09-29 11:18:20 +0530

    Completed

---
+++ TNS_Java_Training/src/com/shreeyash/constructor/studentDeatilsDemo.java
@@ -0,0 +1,10 @@
+package com.shreeyash.constructor;
+
+public class studentDeatilsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		studentDetails s1=new studentDetails("Shreeyash",12,78.00,"A");
+	}
+
+}
---
+++ TNS_Java_Training/src/com/shreeyash/constructor/studentDetails.java
@@ -0,0 +1,12 @@
+package com.shreeyash.constructor;
+
+public class studentDetails {
+	studentDetails(String Name,int rollNumber,double percent,String grade)
+	{
+		System.out.println("==================Parameterized Constructor============");
+		System.out.println("Name is :" +Name);
+		System.out.println("Roll Number is :"+rollNumber);
+		System.out.println("Percentage is :"+percent);
+		System.out.println("Grade is :"+grade);
+	}
+}
